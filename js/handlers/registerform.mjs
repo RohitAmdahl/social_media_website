f7f9a140cd413ef34a61a2065c9c8a9622c2fc4c
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-25 00:08:42 +0100

    update register form

--- js/handlers/registerform.mjs
+++ js/handlers/registerform.mjs
@@ -1,4 +1,6 @@
 const form = document.querySelector("#registerForm");
+const displayMsg = document.querySelector("#displayMsg");
+console.log(displayMsg);
 
 import { register } from "../api/auth/register.mjs";
 
@@ -8,6 +10,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const form = e.target;
+
   const name = form.name.value;
 
   const email = form.email.value;
@@ -15,6 +18,17 @@ form.addEventListener("submit", (e) => {
   const password = form.password.value;
 
   const registerForm = { form, name, email, password };
+
+  setTimeout(() => {
+    displayMsg.style.display = "none";
+  }, 5000);
+  // form.reset();
+  if (form.password.value && form.name.value && form.email.value) {
+    displayMsg.innerHTML = `<p class ="card lead mb-4 m-5 p-2 w-100 text-center">you are successfully registered please log in </p>`;
+    form.reset();
+  } else {
+    return (displayMsg.innerHTML = "");
+  }
 
   register(POSTregisterURL, registerForm);
 });
