4036ecdd36812f5d670b1fd7bb19d2398d617106
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-24 11:09:14 +0100

    update files

--- js/handlers/registerform.mjs
+++ js/handlers/registerform.mjs
@@ -1,1 +1,14 @@
-console.log("object");
+const form = document.querySelector("#registerForm");
+console.log(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+
+  const registerForm = { form, name, email, password };
+  console.log(registerForm);
+  // send it to api
+});
