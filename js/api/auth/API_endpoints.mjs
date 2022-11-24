320a405b030cdcd6d15dae025d63f33d1a1df2a2
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-25 00:09:11 +0100

    api endpoint update

--- js/api/auth/API_endpoints.mjs
+++ js/api/auth/API_endpoints.mjs
@@ -1,9 +1,5 @@
 export const BASE_URL = "https://nf-api.onrender.com";
-
 export const registerURL = "/api/v1/social/auth/register";
 export const POSTregisterURL = `${BASE_URL}${registerURL}`;
-export const LoginAuth = "/api/v1/social/auth/login";
-
-// for register form url
-
+export const LoginAuth = "/api/v1/social/auth/login"; // for register form url
 export const LoginAuthUser = `${BASE_URL}${LoginAuth}`; // login form url
