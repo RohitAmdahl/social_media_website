c80bb325eb99dbec958681a428c9fa4c44c6a516
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-29 18:53:55 +0100

    update

--- js/handlers/create_Post.mjs
+++ js/handlers/create_Post.mjs
@@ -1,7 +1,23 @@
 import { createPost } from "../post/create.mjs";
 import { makePost } from "../api/auth/API_endpoints.mjs";
 
-// const form = document.querySelector("#test");
+const form = document.querySelector("#newPost");
+console.log(form);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const form = e.target.value;
+  const title = form.title.value;
+  const description = form.description.value;
+  const tag = form.tag.value;
+  const picture = form.picture.value;
+  const create_POST = { form, title, description, tag, picture };
+
+  console.log(create_POST);
+
+  createPost(makePost, create_POST);
+});
+
 // const title = document.querySelector(".form-title");
 // const titleValue = title.value;
 // const description = document.querySelector(".form-title");
@@ -14,18 +30,3 @@ import { makePost } from "../api/auth/API_endpoints.mjs";
 
 // const button = document.querySelector(".cta_btn");
 // console.log(button);
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  console.log(object);
-  const form = e.target.value;
-  const title = form.title.value;
-  const description = form.description.value;
-  const tag = form.tag.value;
-  const picture = form.picture.value;
-  const create_POST = { form, title, description, tag, picture };
-
-  console.log(create_POST);
-  testTemplate();
-  createPost(makePost, create_POST);
-});
