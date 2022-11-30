d6f9002189c2bac9ebf70bd7656616c0c7f0a6e3
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-11-30 19:04:48 +0100

    Update specific_post.mjs

--- js/post/specific_post.mjs
+++ js/post/specific_post.mjs
@@ -27,10 +27,53 @@ async function singlePost() {
 
     const card = document.querySelector("#card");
 
+    // const img = document.createElement("img");
+    // img.src =
+
+    const cardItem = document.createElement("div"); // card
+    cardItem.classList.add("card", "m-5", "p-5");
+
+    const image = document.createElement("img"); // image
+    image.classList.add("d-flex", "img-thumbnail", "p-2", "mb-2", "mx-auto");
+    // image.setAttribute("src", link.author.avatar);
+
+    const newCard = document.createElement("div"); // card-body
+    newCard.className = "card-body";
+    const NameOfTittle = document.createElement("h5");
+    // NameOfTittle.className =  ;
+
+    NameOfTittle.classList.add("mb-2", "p-5", "m-5", "card-title");
+    NameOfTittle.innerText = link.title;
+    const subtitle = document.createElement("h6");
+
+    subtitle.classList.add("card-subtitle", "mb-2", "p-2", "text-muted");
+    const paraGraph_text = document.createElement("p");
+    paraGraph_text.className = "card-text";
+    paraGraph_text.classList.add("mb-2", "p-2", "mb-2", "card-text");
+    // paraGraph_text.innerText = link.body;
+
     const img = document.createElement("img");
-    img.src = link.media;
+    // img.setAttribute("src", link.media);
+    img.classList.add(
+      "d-flex",
+      "img-thumbnail",
+      "mb-2",
+      "p-2",
+      "mb-2",
+      "media-img"
+    );
+    const comments = document.createElement("p");
+    // comments.className = "comments" + items.comments.length;
+    const button = document.createElement("button");
+    const input = document.createElement("input");
+    input.classList.add("form-control", "mb-2");
+
+    button.classList.add("cta_btn", "m-5");
+    button.innerText = "comment";
+    console.log(button);
 
     card.appendChild(img);
+    card.appendChild(cardItem);
 
     //----
     //---
