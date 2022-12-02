ee967541f3c6d44ab0d912000760c815c3a6fb3b
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-02 21:34:17 +0100

    update

--- js/handlers/updateHandeler.mjs
+++ js/handlers/updateHandeler.mjs
@@ -10,24 +10,25 @@ function updatePostListener() {
   console.log(url);
   const id = url.searchParams.get("id");
   console.log(id);
+  if (from) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      console.log(e);
+      const form = e.target;
+      const title = form[0].value;
+      console.log(title);
+      const body = form[1].value;
+      console.log(body);
+      const image = form[2].value;
+      console.log(image);
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log(e);
-    const form = e.target;
-    const title = form[0].value;
-    console.log(title);
-    const body = form[1].value;
-    console.log(body);
-    const image = form[2].value;
-    console.log(image);
-
-    // const id = form.post.id;
-    // console.log(id);
-    const update_POST = { form, title, body, image, id };
-    console.log(update_Post);
-    updatePost(`${UPDATE_URL}${post.id}`, update_POST);
-  });
+      const id = form.post.id;
+      // console.log(id);
+      const update_POST = { form, title, body, image, id };
+      console.log(update_Post);
+      updatePost(`${UPDATE_URL}${post.id}`, update_POST);
+    });
+  }
 }
 updatePostListener();
 
---
+++ js/post/profile.mjs
@@ -0,0 +1,60 @@
+import { getFollowProfilelink } from "../api/auth/API_endpoints.mjs";
+
+import { BASE_URL } from "../api/auth/API_endpoints.mjs";
+
+const cardModel = document.getElementById("cards");
+
+const token = localStorage.getItem("Token");
+const profile = localStorage.getItem("profile");
+
+console.log("Profile", JSON.parse(profile).name);
+
+const form = document.getElementById("edit");
+console.log(form);
+
+async function Post(url) {
+  try {
+    const createData = {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      // body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, createData);
+    const results = await response.json();
+    console.log("results", results);
+
+    form.innerHTML += `
+    <div>Create post</div>
+    <div>
+      <label for="title" class="text-muted">Title</label>
+      <input name="title" type="text" class="form-control" value="${results[0].title}" required />
+    </div>
+    <div>
+      <label for="textpost" class="text-muted">Description</label>
+      <textarea name="text" id="text" cols="30" rows="2" class="form-control">
+      ${results[0].body}
+      </textarea>
+    </div>
+
+    <div>
+      <input name="id" type="hidden" class="form-control" required />
+    </div>
+    <div>
+      <label for="picture">Picture</label>
+      <input name="media" type="url" id="file" name="file" class="form-control" />
+    </div>
+
+    <button class="cta_btn_profile" type="submit" data-postid=${results[0].id} >
+      update Post
+    </button>`;
+  } catch (error) {
+    console.log("error", error);
+  }
+}
+
+Post(`${BASE_URL}/api/v1/social/profiles/${JSON.parse(profile).name}`);
+Post(`${BASE_URL}/api/v1/social/profiles/${JSON.parse(profile).name}/posts`);
