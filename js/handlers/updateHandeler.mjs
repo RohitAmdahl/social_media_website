9a72352264e7a271fd28c931d6c64823106bbc75
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-02 14:28:53 +0100

    update

--- js/handlers/updateHandeler.mjs
+++ js/handlers/updateHandeler.mjs
@@ -2,10 +2,16 @@ import { makePost } from "../api/auth/API_endpoints.mjs";
 import { updatePost } from "../post/update.mjs";
 import { UPDATE_URL } from "../api/auth/API_endpoints.mjs";
 
-console.log(updatePost);
+const update_post_data = document.querySelector(".edit_data");
+console.log(form);
+
+// const newForm = document.createElement("div"); // card-body
+// const form = document.createElement("form"); // form
+// const edit_post = document.createElement("div"); // form
 
 function updatePostListener() {
-  const form = document.getElementById("edit");
+  const form = document.querySelector(".edit");
+  console.log(form);
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -13,11 +19,16 @@ function updatePostListener() {
     console.log(e);
     const form = e.target;
     const title = form[0].value;
+    console.log(title);
     const body = form[1].value;
+    console.log(body);
     const image = form[2].value;
+    console.log(image);
 
-    const id = Number(form[3].dataset.post.id);
+    // const id = Number(form[3].dataset.post.id);
+    // console.log(id);
     const update_POST = { form, title, body, image, id };
+    console.log(update_Post);
     updatePost(`${UPDATE_URL}${post.id}`, update_POST);
   });
 }
--- js/post/update.mjs
+++ js/post/update.mjs
@@ -1,35 +1,35 @@
-// import { UPDATE_URL } from "../api/auth/API_endpoints.mjs";
-// console.log(UPDATE_URL);
-// const method = "put";
+import { UPDATE_URL } from "../api/auth/API_endpoints.mjs";
+console.log(UPDATE_URL);
+const method = "put";
 
-// export async function updatePost(url, post) {
-//   console.log(post);
+export async function updatePost(url, post) {
+  console.log(post);
 
-//   try {
-//     const token = localStorage.getItem("Token");
-//     console.log(token);
+  try {
+    const token = localStorage.getItem("Token");
+    console.log(token);
 
-//     const updateData = {
-//       method,
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${token}`,
-//       },
+    const updateData = {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
 
-//       body: JSON.stringify({
-//         title: post.title,
-//         body: post.body,
-//         media: post.image,
-//          id: post.id,
-//       }),
-//     };
+      body: JSON.stringify({
+        title: post.title,
+        body: post.body,
+        media: post.image,
+        // id: post.id,
+      }),
+    };
 
-//     const response = await fetch(`${UPDATE_URL}${post.id}`, updateData);
-//     console.log(response);
-//     const json = await response.json();
-//     console.log(json);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// //..................................................................
+    const response = await fetch(`${UPDATE_URL}${post.id}`, updateData);
+    console.log(response);
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+}
+//..................................................................
