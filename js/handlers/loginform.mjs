ba1c3a167a79d72c95b3fe9706de4f886b67b653
Rohit kumar Amdahl <rohit_designer@outlook.com>
2022-12-10 01:59:04 +0100

    update

--- js/handlers/loginform.mjs
+++ js/handlers/loginform.mjs
@@ -1,15 +1,29 @@
 import { LoginAuthUser } from "../api/auth/API_endpoints.mjs";
 console.log(LoginAuthUser);
 import { login } from "../api/auth/login.mjs";
-const form = document.querySelector("#loginForm");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const form = e.target;
-  const email = form.email.value;
-  const password = form.password.value;
-  const loginForm = { form, email, password };
-  form.reset();
+function logINform() {
+  const form = document.querySelector("#loginForm");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const loginForm = { form, email, password };
+    form.reset();
 
-  login(LoginAuthUser, loginForm);
-});
+    login(LoginAuthUser, loginForm);
+  });
+}
+
+logINform();
+
+// function replacePage() {
+//   const logIn = document.getElementById("logIn");
+//   console.log("object");
+//   logIn.addEventListener("click", (e) => {
+//     window.location.replace("/profile.html");
+//   });
+// }
+
+// replacePage();
